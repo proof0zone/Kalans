15da4010e25e0a02ebd9cd193b83c93498960c36
Rares Antalut <proof_zone@yahoo.com>
2017-09-30 14:07:24 +0300

    KL-00002: Table format and t.header text added.
    created th_text for the table header and partially implemented the format for each cell.
    Time Spent 1:30-2:10(PM) - 0h:40min

--- src/generate_table_ivps.py
+++ src/generate_table_ivps.py
@@ -2,11 +2,44 @@
 from openpyxl import Workbook
 wb = Workbook() # current workbook
 ws = wb.active  # default worksheet
-
+#==========================================#==========================================
+# STAGE ONE
+# CREATE THE TABLE AND FORMAT IT
+#==========================================#==========================================
 #A1:X1 (CAP TABEL)
 ws.merge_cells("A1:X1")
-ct = ws["A1"]
+#A2:A3 diafrag
+ws.merge_cells("A2:A3")
+#B2:B3 SPALETI
+ws.merge_cells("B2:B3")
+
+
+
 measure_units = {'m1': 'm', 'm2': 'm^2'}
-th_text = {'A1': 'ÎNCĂRCĂRI VERTICALE PE ȘPALEȚI',}
+th_text = {'A1': 'ÎNCĂRCĂRI VERTICALE PE ȘPALEȚI', \
+           'A2':'Diafrag',\
+           'B2':'',\
+           'C2':'',\
+           'D2':'',\
+           'E2':'',\
+           'F2':'',\
+           'G2':'',\
+           'H2':'',\
+           'I2':'',\
+           'J2':'',\
+           'K2':'',\
+           'L2':'',\
+           'M2':'',\
+           'N2':'',\
+           'O2':'',\
+           'P2':'',\
+           'Q2':'',\
+           'R2':'',\
+           'S2':'',\
+           'T2':'',\
+           'U2':'',\
+           }
+
+ws['A1'] = th_text['A1']
 
 wb.save("ivps.xlsx")
